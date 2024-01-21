6926908d52f61d7a4d14de1811f0bc6983d6ff72
Nguyễn Phúc <phucfe050203@gmail.com>
2024-01-21 17:53:36 +0700

    Config Project 1

--- .history/src/App_20240121171633.js
+++ .history/src/App_20240121171633.js
@@ -1,4 +1,5 @@
-
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
     return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
---
+++ .history/src/router/routing_20240121173350.js
@@ -0,0 +1,65 @@
+import { redirect, createBrowserRouter, useNavigate, Route } from 'react-router-dom'
+import ComponentPage from '../Pages/ComponentPage'
+import MainLayout from '../Layout/MainLayout/MainLayout'
+import { APP_ROUTER } from '../Utils/Constants'
+import HomePage from '../Pages/Home/HomePage'
+import ProductPage from '../Pages/ProductPage/ProductPage'
+import OrderPage from '../Pages/OrderPage/OrderPage'
+import UserPage from '../Pages/UserPage/UserPage'
+import AuthLayout from '../Layout/AuthLayout/AuthLayout'
+import Login from '../Pages/Auth/Login'
+import Register from '../Pages/Auth/Register'
+import { loaderUser } from './loader/loaderAuth'
+import CategoryPage from '../Pages/CategoryPage/CategoryPage'
+
+const router = createBrowserRouter([
+    {
+        path: APP_ROUTER.INDEX,
+        element: <MainLayout />,
+        loader: (request) => loaderUser(request),
+        children: [
+            {
+                path: APP_ROUTER.HOME,
+                element: <HomePage />,
+                index: true,
+            },
+            {
+                path: APP_ROUTER.PRODUCT,
+                element: <ProductPage />,
+            },
+            {
+                path: APP_ROUTER.CATEGORY,
+                element: <CategoryPage />,
+            },
+            {
+                path: APP_ROUTER.ORDER,
+                element: <OrderPage />,
+            },
+            {
+                path: APP_ROUTER.USER,
+                element: <UserPage />,
+            },
+            {
+                path: APP_ROUTER.COMPONENT,
+                element: <ComponentPage />,
+            },
+        ],
+    },
+    {
+        path: APP_ROUTER.AUTH,
+        element: <AuthLayout />,
+        children: [
+            {
+                path: APP_ROUTER.LOGIN,
+                element: <Login />,
+                index: true,
+            },
+            {
+                path: APP_ROUTER.REGISTER,
+                element: <Register />,
+            },
+        ],
+    },
+])
+
+export default router
